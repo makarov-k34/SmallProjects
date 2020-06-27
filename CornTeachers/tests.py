6453968398e496ea748f1ff39660a08b6e6080bd
makarov-k34 <67468608+makarov-k34@users.noreply.github.com>
2020-06-27 13:49:30 +0300

    Не ту версию вчера залил в теты

--- CornTeachers/tests.py
+++ CornTeachers/tests.py
@@ -1,8 +1,25 @@
 import unittest
 
+from pyramid.config import Configurator
+from webtest import TestApp
 from pyramid import testing
 
+
+def main(global_config, **settings):
+    config = Configurator(settings=settings)
+    config.include("cornice")
+    config.scan("CornTeachers.views")
+    return config.make_wsgi_app()
+
 class Views(unittest.TestCase):
+
+    def setUp(self):
+        app = main({})
+        self.m_testApp = TestApp(app)
+        self.config = testing.setUp()
+
+    def tearDown(self):
+        testing.tearDown()
 
     def get_response(self, uri: str):
         self.response = self.m_testApp.get(uri, headers={"usr-timezone": '3'})
